c5fb130c65133639a27ec1951fe246049055cd07
juan-rybczinski <jhreplay.lee@gmail.com>
2024-01-14 19:25:14 +0900

    타입과 값으로 모두 사용 가능한 Class

---
+++ 9_class/4_class_as_type_and_value.ts
@@ -0,0 +1,23 @@
+class Dog {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  bark() {
+    return `${this.name}가 짖습니다!`;
+  }
+}
+
+let ori = new Dog('오리');
+console.log(ori.bark());
+
+// ori = '오리';
+
+ori = {
+  name: '별이',
+  bark(): string {
+    return `${this.name}가 짖습니다!`;
+  },
+};
