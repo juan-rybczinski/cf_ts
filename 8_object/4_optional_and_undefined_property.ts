214474c6e7c224ab65a4666dd20ba3c42276983b
juan-rybczinski <jhreplay.lee@gmail.com>
2024-01-14 09:05:21 +0900

    Optional vs Undefined 프로퍼티

---
+++ 8_object/4_optional_and_undefined_property.ts
@@ -0,0 +1,28 @@
+interface Dog {
+  name: string;
+  age: number;
+  breed?: string;
+}
+
+const byulE: Dog = {
+  name: '별이',
+  age: 12,
+  breed: '미니핀',
+};
+
+const ori: Dog = {
+  name: '오리',
+  age: 3,
+};
+
+interface Cat {
+  name: string;
+  age: number;
+  breed: string | undefined;
+}
+
+const nabi: Cat = {
+  name: '나비',
+  age: 7,
+  breed: undefined,
+};
