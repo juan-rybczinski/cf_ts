c7c19e923bfac2a7156e716119e2e9e79c678cc2
juan-rybczinski <jhreplay.lee@gmail.com>
2024-01-16 22:46:05 +0900

    ConstructorParameters Type

---
+++ 11_utility_type/10_constructor_parameter.ts
@@ -0,0 +1,11 @@
+class Idol {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+type ConstructorParamType = ConstructorParameters<typeof Idol>;
