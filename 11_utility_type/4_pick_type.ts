b37a2a6dd052ce2a9da24d090057c7f979588407
juan-rybczinski <jhreplay.lee@gmail.com>
2024-01-16 22:36:07 +0900

    Pick Type

---
+++ 11_utility_type/4_pick_type.ts
@@ -0,0 +1,15 @@
+interface Post {
+  title: string;
+  content: string;
+  createdAt: Date;
+}
+
+const createPost = (post: Pick<Post, 'title' | 'content'>) => ({
+  ...post,
+  createdAt: new Date(),
+});
+
+console.log(createPost({
+  title: '요즘 개발자 취업 어떤가요?',
+  content: '나이 30 넘어서도 해볼만한가요?',
+}));
