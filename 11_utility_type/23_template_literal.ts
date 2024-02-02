777cc0e7854b7952becdb6f7a665fcd19e2aac61
juan-rybczinski <jhreplay.lee@gmail.com>
2024-02-02 15:40:52 +0900

    Template Literal Types

---
+++ 11_utility_type/23_template_literal.ts
@@ -0,0 +1,6 @@
+type CodeFactory = 'Code Factory';
+
+type CodeFactoryUpper = Uppercase<CodeFactory>;
+type CodeFactoryLower = Lowercase<CodeFactoryUpper>;
+type CodeFactoryCapital = Capitalize<CodeFactoryLower>;
+type CodeFactoryUncapital = Uncapitalize<CodeFactoryCapital>;
